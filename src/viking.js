9f81e7fa9c622e8ce770be6bdfa9628557ce7fb3
Lea Model <lea.skywalker309@gmail.com>
2023-06-16 12:33:22 +0200

    Saxon and War added. Finished addViking addSaxon method in War

--- src/viking.js
+++ src/viking.js
@@ -37,7 +37,37 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength){
+        super(health, strength);
+    };
+
+    receiveDamage(damage) {
+        this.health -= damage;
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        } else {
+            return `A Saxon has died in combat`
+        }
+    };
+}
 
 // War
-class War {}
+class War {
+    constructor (){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking (viking){
+        this.vikingArmy.push(viking);
+    }
+
+    addSaxon (saxon){
+        this.saxonArmy.push(saxon);
+    }
+
+    vikingAttack() {
+        
+    }
+}

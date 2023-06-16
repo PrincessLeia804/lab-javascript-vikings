6dad1f5b731eedc4c3036a6b4287a07f07a2d7b8
Lea Model <lea.skywalker309@gmail.com>
2023-06-16 16:18:08 +0200

    Lab finished

--- src/viking.js
+++ src/viking.js
@@ -124,6 +124,14 @@ class War {
         } 
         return fightResult;
     }
-
+    showStatus() {
+        if (this.saxonArmy.length === 0){
+            return "Vikings have won the war of the century!";
+        } else if (this.vikingArmy.length === 0){
+            return "Saxons have fought for their lives and survived another day...";
+        } else if (this.vikingArmy.length > 0 && this.saxonArmy.length > 0) {
+            return "Vikings and Saxons are still in the thick of battle.";
+        }
+    }
 
 }
